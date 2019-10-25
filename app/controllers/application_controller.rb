ffc7fe6aebc4b0016c1dd0037e68356b7159a255
Mark Hernandez <m.hern1415@gmail.com>
2019-10-25 19:48:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,21 +16,6 @@ class ApplicationController < Sinatra::Base
 		erb :signup
 	end
 
-	post "/signup" do
-		user = User.new(:username => params[:username], :password => params[:password])
-
-		if user.save
-			redirect '/login'
-		else
-			redirect '/failure'
-		end
-	end
-
-
-	get "/login" do
-		erb :login
-	end
-
   post "/signup" do
 
     if params[:username] == "" || params[:password] == ""
@@ -41,6 +26,21 @@ class ApplicationController < Sinatra::Base
       redirect to "/login"
     end
   end
+
+
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
+	end
 
 	get "/success" do
 		if logged_in?
